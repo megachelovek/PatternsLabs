d115272e1b9905e751219b29990e3c7d62b2f921
megachelovek <2regvk@mail.ru>
2019-03-13 18:52:33 +0400

    #master feat(PatternsLabs): DAO Method

--- src/main/java/com/ssau/ReaderDAOserializable.java
+++ src/main/java/com/ssau/ReaderDAOserializable.java
@@ -9,7 +9,6 @@ public class ReaderDAOserializable implements DataDAO {
     @Override
     public List<Student> GetStudentsFromFile(String path) {
         List<Student> students = null;
-        List<Student> newStudents = new ArrayList<Student>();
         try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path)))
         {
             students= (ArrayList<Student>) ois.readObject();
@@ -17,16 +16,6 @@ public class ReaderDAOserializable implements DataDAO {
         catch(Exception ex){
         }
 
-        for (int i = 0; i <students.size(); i++)
-        {
-            int[] tt = students.get(i).getMarks();
-            Student tempStudent = new Student(students.get(i).getName(), 0);
-            for (int j = 0; j < students.get(i).getLength(); j++) {
-                tempStudent.addSubjectAndMark(students.get(i).getSubject(j),tt[i]);
-            }
-            newStudents.add(tempStudent);
-        }
-
-        return newStudents;
+        return students;
     }
 }
